67ce49431a3a076e04ae68f56eb86d7695e1a2ff
Emily Nox <ennox00@gmail.com>
2024-09-18 01:37:12 -0700

    Added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,25 @@ const montserrat = Montserrat({subsets: ["latin"]});
 export const metadata: Metadata = {
   title: "E.N. Nox",
   description: "Emily Nox is an experienced software engineer who enjoys working the full stack from complex backends to responsive frontends.",
+  icons: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      url: '/favicon-32x32.png'
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      url: '/favicon-16x16.png'
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      url: '/apple-touch-icon.png'
+    }
+  ]
 };
 
 export const viewport: Viewport = {
